9f80b56ef2140287979f948bf4edd0a3c743b215
Karolusrex <kalle.lundfall@gmail.com>
2019-04-25 09:49:14 +0200

    chore: format code

--- js/shapeshifter.es6.js
+++ js/shapeshifter.es6.js
@@ -1,9 +1,8 @@
-const trigger = document.getElementById('load-shapeshifter');
-const iframe = document.getElementById('shapeshifter-iframe');
+const trigger = document.getElementById("load-shapeshifter");
+const iframe = document.getElementById("shapeshifter-iframe");
 
-
-trigger.addEventListener('click', () => {
-    document.documentElement.requestFullscreen && document.documentElement.requestFullscreen();
-    iframe.src = 'https://shape-shifter-app.firebaseapp.com';
-})
-
+trigger.addEventListener("click", () => {
+  document.documentElement.requestFullscreen &&
+    document.documentElement.requestFullscreen();
+  iframe.src = "https://shape-shifter-app.firebaseapp.com";
+});
