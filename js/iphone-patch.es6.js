64538db6ec91388cf1a1dd6bb47c254b90a9e523
Karolusrex <kalle.lundfall@gmail.com>
2019-04-26 17:13:14 +0200

    fix: replace ad-hoc iphone patch with (giant...) fastclick library

--- js/iphone-patch.es6.js
+++
@@ -1,5 +0,0 @@
-if(!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)){
-    [...document.getElementsByTagName('a')].forEach(element => {
-        element.removeAttribute('target');
-    });
-}
